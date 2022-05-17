7abb970d99fc6585d88ebc35e21983e9c2ae9bde
bodascaluca <bodasca.luca@gmail.com>
2022-05-17 10:57:00 +0200

    Forse il problema è stato risolto

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <TitleTest />
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -7,11 +8,13 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import TitleTest from "./components/TitleTest.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    TitleTest,
   },
 };
 </script>
---
+++ src/components/TitleTest.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <h1>La mia prima app con Vue CLI</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: `TitleTest`,
+};
+</script>
+
+<style scoped></style>
